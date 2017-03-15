b72971ea6038b9bcd528e54e5f5a18c98bdde0f3
Jason Barbee <jason.barbee@gmail.com>
2017-03-15 10:22:01 -0500

    UCS Add to Vnic template "functional" but could use error handling.

--- library/ucs_vlan_vnic.py
+++ library/ucs_vlan_vnic.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+# TODO Error handling.
+# Detect parameters missing. Ansible does this right?...
+# Merge add/remove
 
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'Jason Barbee',
@@ -6,35 +9,24 @@ ANSIBLE_METADATA = {'status': ['preview'],
 
 DOCUMENTATION = '''
 ---
-module: ucs_vlan_to_pc
+module: ucs_vlan_vnic
 version_added: "2.2"
-short_description: Adds Vlan to Port Channels. Name and ID must match exactly
+short_description: Adds Vlan to VNIC Template. Vlan Name must match exactly
 description:
-    - Adds or removes Vlans from Port Channels. Name and ID must match exactly
+    - Adds Vlan to VNIC Template. Vlan Name must match exactly
 author: Jason Barbee (@jasonbarbee)
 extends_documentation_fragment: ucs
 options:
-    vlan_id:
+    vnic_template:
         description:
-            - Single VLAN ID.
-        required: false
-        default: null
-    pc_id:
-        description:
-            - Port Channel ID
-        required: false
-        default: null
-    vlan_name:
-        description:
-            - Vlan name, include the seperator you want between the name and id.
-              Example vlan_ID or vlan-ID.  vlan_id is appended to this value.
+            - Single VNIC Template Name
         required: true
         default: null
     vlan_name:
         description:
-            - admin speed. 1gbps 10gbps or 40gpbs
-        required: false
-        default: 10gbps
+            - Vlan name
+        required: true
+        default: null
     state:
         description:
         - Desired State of the action - present / absent
@@ -44,42 +36,38 @@ options:
 '''
 
 EXAMPLES = '''
-- name: Adding Single Vlan ID {{vlan_id}} to {{ pc_id} on UCS:{{ucsm_ip}}
-    ucs_vlan_add_to_pc:
+- name: Adding Single Vlan ID to Template
+    ucs_vlan_add_to_vnic:
       ip={{ucsm_ip}}
       login={{ucsm_login}}
       password={{ucsm_pw}}
-      vlan_name='vlan_'
-      vlan_id='10'
-      pc_id='1'
+      vlan_name=''
+      vnic_template:'vnic-template'
 
-- name: Adding Single Vlan ID {{vlan_id}} to {{ pc_id} on UCS:{{ucsm_ip}}
-    ucs_vlan_remove_from_pc:
+- name: Remove Single Vlan ID to Template
+    ucs_vlan_remove_from_vnic:
       ip={{ucsm_ip}}
       login={{ucsm_login}}
       password={{ucsm_pw}}
       vlan_name='servers'
-      vlan_id='10'
-      pc_id='1'
+      vnic_template:'vnic-template'
 '''
 
-
-
+from ucsmsdk.mometa.vnic.VnicLanConnTempl import VnicLanConnTempl
+from ucsmsdk.mometa.vnic.VnicEtherIf import VnicEtherIf
 from ucsmsdk.mometa.fabric.FabricEthLan import FabricEthLan
 from ucsmsdk.mometa.fabric.FabricVlan import FabricVlan
-from ucsmsdk.mometa.fabric.FabricEthVlanPc import FabricEthVlanPc
+
 from library.ucs import UCS
 
 
-def ucs_add_vlan_to_pc(module):
+def ucs_add_vlan_to_vnic(module):
     vlan_name = module.params.get('vlan_name')
-    vlan_id = module.params.get('vlan_id')
     ucsm_ip = module.params.get('ip')
     ucsm_pw = module.params.get('password')
     ucsm_login = module.params.get('login')
-    pc_id = module.params.get('pc_id')
-    admin_speed = module.params.get('admin_speed')
-    is_native = module.params.get('is_native')
+    vnic_template = module.params.get('vnic_template')
+    default_net = module.params.get('default_net')
 
     ucsm = UCS(ucsm_ip, ucsm_login, ucsm_pw)
 
@@ -94,14 +82,11 @@ def ucs_add_vlan_to_pc(module):
 
     FILIST = ['A', 'B']
     for FI in FILIST:
-        obj = ucsm.handle.query_dn("fabric/lan")
         try:
-            mo = FabricEthLan(parent_mo_or_dn=obj, id=FI)
-            mo_1 = FabricVlan(parent_mo_or_dn=mo, sharing="none", name=vlan_name, id=vlan_id, mcast_policy_name="", policy_owner="local", default_net="no", pub_nw_name="", compression_type="included")
-            mo_1_1 = FabricEthVlanPc(parent_mo_or_dn=mo_1, name=vlan_name, descr="", is_native=is_native, admin_speed=admin_speed, switch_id=FI, admin_state="enabled", oper_speed=admin_speed, port_id=pc_id)
-            ucsm.handle.add_mo(mo, modify_present=True)
+            mo = VnicLanConnTempl(parent_mo_or_dn="org-root", redundancy_pair_type="none", nw_ctrl_policy_name="", descr="", stats_policy_name="default", admin_cdn_name="", switch_id=FI, pin_to_group_name="", mtu="1500", policy_owner="local", peer_redundancy_templ_name="", templ_type="updating-template", qos_policy_name="", ident_pool_name="", cdn_source="vnic-name", name=vnic_template)
+            mo_1 = VnicEtherIf(parent_mo_or_dn=mo, default_net=default_net, name=vlan_name)
+            ucsm.handle.add_mo(mo, True)
 
-            ucsm.handle.commit()
             results['changed'] = True
 
         except Exception as e:
@@ -120,16 +105,12 @@ def ucs_add_vlan_to_pc(module):
 
     return results
 
-def ucs_remove_vlan_from_pc(module):
+def ucs_remove_vlan_from_vnic(module):
     vlan_name = module.params.get('vlan_name')
-    vlan_id = module.params.get('vlan_id')
     ucsm_ip = module.params.get('ip')
     ucsm_pw = module.params.get('password')
     ucsm_login = module.params.get('login')
-    pc_id = module.params.get('pc_id')
-    admin_speed = module.params.get('admin_speed')
-    is_native = module.params.get('is_native')
-
+    vnic_template = module.params.get('vnic_template')
     ucsm = UCS(ucsm_ip, ucsm_login, ucsm_pw)
 
     results = {}
@@ -143,17 +124,9 @@ def ucs_remove_vlan_from_pc(module):
 
     FILIST = ['A','B']
     for FI in FILIST:
-        obj = ucsm.handle.query_dn("fabric/lan")
-        lancloud = ucsm.handle.query_classid(class_id="FabricLanCloud") 
         try:
-            mo = FabricEthLan(parent_mo_or_dn=obj, id=FI)
-            mo_1 = FabricVlan(parent_mo_or_dn=mo, sharing="none", name=vlan_name, id='vlan_id', mcast_policy_name="", policy_owner="local", default_net="no", pub_nw_name="", compression_type="included")
-            mo_1_1 = ucsm.handle.query_children(
-                in_mo=mo_1,
-                class_id="FabricEthVlanPc",
-                filter_str='(vlan_name + + str(vlan_id), %s, type="eq")' % (vlan_nane,vlan_id)
-    	 	)  
-            #mo_1_1 = FabricEthVlanPc(parent_mo_or_dn=mo_1, name=vlan_name + + str(vlan_id), descr="", is_native=is_native, admin_speed=admin_speed, switch_id=FI, admin_state="enabled", oper_speed=admin_speed, port_id=pc_id)
+            mo_1 = ucsm.handle.query_dn("org-root/lan-conn-templ-test-template/if-"+vlan_name)
+            ucsm.handle.remove_mo(mo_1, True)
 
             ucsm.handle.remove_mo(mo_1_1)
             ucsm.handle.commit()
@@ -178,35 +151,31 @@ def ucs_remove_vlan_from_pc(module):
 def main():
     module = AnsibleModule(
         argument_spec     = dict(
-        pc_id             = dict(required=True),
-        vlan_id           = dict(required=True),
         vlan_name         = dict(required=True),
         ip                = dict(required=True),
         password          = dict(required=True),
         login             = dict(required=True),
-        admin_speed       = dict(required=False, default='10gbps'),
-        is_native         = dict(required=False),
+        vnic_template     = dict(required=True),
+        default_net       = dict(required=False, default='no', choices=['yes','no']),
     	state 		      = dict(required=False, default='present',choices=['present', 'absent']),
         )
     )
 
-    vlan_id = module.params.get('vlan_id')
-    pc_id = module.params.get('pc_id')
     state = module.params.get('state')
     vlan_name = module.params.get('vlan_name')
 
-    if pc_id:
+    if vlan_name:
         if state == 'present':
             if len(vlan_name) <= 16:
-                results = ucs_add_vlan_to_pc(module)
+                results = ucs_add_vlan_to_vnic(module)
                 module.exit_json(**results)
             else:
                 module.fail_json(msg='Vlan Name must be 16 char or less')
         else:
-                results = ucs_remove_vlan_from_pc(module)
+                results = ucs_remove_vlan_from_vnic(module)
                 module.exit_json(**results)
     else:
-        module.fail_json(msg='Missing port channel id.')
+        module.fail_json(msg='Missing vlan name.')
 
 
 from ansible.module_utils.basic import *
